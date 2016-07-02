0fe39d1b1f3f39b5385b97655f5571cfa66cf98e
DrewStock <andrewdstock@gmail.com>
2016-07-02 15:38:57 -0700

    added do while loop to force responses and implemented score counter

--- game.js
+++ game.js
@@ -1,24 +1,62 @@
+do {
 var userName = prompt("Welcome to Drew's Guessing Game! What's your name?","");
-console.log("Start of the game. The user's name is: "+userName);
-alert("It's great to meet you, "+userName+"! Let's get started.");
+  if (userName == "") {
+  alert("Don't be a stranger! Please tell me your name.");
+  }
+}
+while (userName == "");
+console.log("Start of the game. The user's name is: " + userName);
+alert("It's great to meet you, " + userName + "! Let's get started.");
+do {
 var state = prompt("Did I go to college in Pennsylvania? Please answer Yes or No.","").toUpperCase();
+  if (state == "") {
+  alert("Come on, " + userName +". Play nice and answer the question.");
+  }
+}
+while (state == "");
 console.log("First question. The user's answer was: "+state);
-if (state === 'YES') {
-  alert("Good guess, "+userName+"! I certainly did go to college in Pennsylvania.\nI'm a graduate of Penn State University, which is located in State College, PA.");
+var score = 0;
+if (state === 'YES' || state === 'Y') {
+  alert("Good guess, " + userName + "! I certainly did go to college in Pennsylvania.\nI'm a graduate of Penn State University, which is located in State College, PA.");
+  score += 1;
 } else {
-  alert("Sorry, "+userName+". That's incorrect. I actually did go to college in Pennsylvania.");
+  alert("Sorry, " + userName + ". That's incorrect. I actually did go to college in Pennsylvania.");
+  score += 0;
 }
+console.log("User's score is: " + score);
+do {
 var timbers = prompt("Am I a fan of the Portland Timbers? Please answer Yes or No.","").toUpperCase();
-console.log("Second question. The user's answer was: "+timbers);
-if (timbers === 'YES') {
-  alert("That's right, "+userName+". You bet I am!\nKeep it up, Rose City! Don't let up, no pity!");
-} else {
-  alert("Dang, "+userName+". That's dead wrong.\nYou didn't think that I was a Sounders fan, did you?");
+  if (timbers == "") {
+  alert("Come on, " + userName +". Play nice and answer the question.");
+  }
 }
+while (timbers == "");
+console.log("Second question. The user's answer was: " + timbers);
+if (timbers === 'YES' || timbers === 'Y') {
+  alert("That's right, " + userName + ". You bet I am!\nKeep it up, Rose City! Don't let up, no pity!");
+  score += 1;
+} else {
+  alert("Dang, " + userName + ". That's dead wrong.\nYou didn't think that I was a Sounders fan, did you?");
+  score += 0
+}
+console.log("User's score is: " + score);
+do {
 var beer = prompt("Do I like beer? Please answer Yes or No.","").toUpperCase();
-console.log("Final question. The user's answer was: "+beer);
-if (beer === 'YES') {
-  alert("Trick question, "+userName+"! I don't just like beer, I LOVE beer!");
-} else {
-  alert("You know me too well, "+userName+".\nYou realized that I was trying to fool you, as my feelings about beer are so much stronger than simply just liking it.\nYou've earned my respect and admiration, cheers!");
+  if (beer == "") {
+    alert("Come on, " + userName +". Play nice and answer the question.");
+  }
 }
+while (beer == "");
+console.log("Third question. The user's answer was: " + beer);
+if (beer === 'YES' || beer === 'Y') {
+  alert("Trick question, " + userName + "! I don't just like beer, I LOVE beer! Sorry, you answered incorrectly.");
+  score += 0;
+} else if (beer === 'NO' || beer === 'N') {
+  alert("You know me too well, " + userName + ".\nYou realized that I was trying to fool you, as my feelings about beer are so much stronger than simply just liking it.\nYou've earned my respect and admiration, cheers!");
+  score += 1;
+} else {
+  alert("Sorry, " + userName + ". That's incorrect.");
+  score += 0;
+}
+console.log("User's score is: " + score);
+alert("Thanks for playing, " + userName + ". You answered " + score + " out of 3 questions correctly.")
