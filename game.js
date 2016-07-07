d9717ad49633ac428e61cf99ab1c010f5e634fcd
DrewStock <andrewdstock@gmail.com>
2016-07-07 14:10:44 -0700

    updated to write initial response (to prompt for username) to document

--- game.js
+++ game.js
@@ -1,20 +1,31 @@
+
+var userName = "";
+
+function getStarted () {
 do {
-  var userName = prompt("Welcome to Drew's Guessing Game! What's your name?","");
+  userName = prompt("Welcome to Drew's Guessing Game! What's your name?","");
   if (userName == "") {
   alert("Don't be a stranger! Please tell me your name.");
   }
 }
 while (userName == "");
-alert("It's great to meet you, " + userName + "! Let's get started.");
+var response = document.getElementById("response");
+response.innerHTML = "It's great to meet you, " + userName + "! Let's get started.";
+return userName;
+}
 
 var score = 0;
+function playGame () {
 
 // Function to ask yes/no questions
 function ask (question, affirmative, negative) {
+do {
   var q = prompt(question,"").toUpperCase();
   if (q == "") {
     alert("Come on. Play nice and answer the question.");
   }
+}
+while (q == "");
   if (q === 'YES' || q === 'Y') {
     alert(affirmative);
     score += 1;
@@ -26,6 +37,9 @@ function ask (question, affirmative, negative) {
 }
 
 ask("Did I go to college in Pennsylvania? Please answer Yes or No.","Good guess! I certainly did go to college in Pennsylvania. I'm a graduate of Penn State University, which is located in State College, PA.","Sorry, that's incorrect. I actually did go to college in Pennsylvania.");
+
 ask("Am I a fan of the Portland Timbers? Please answer Yes or No.","You bet I am! Keep it up, Rose City! Don't let up, no pity!","Dang, that's dead wrong. You didn't think that I was a Sounders fan, did you?");
 ask("Do I love beer? Please answer Yes or No.","That is correct.","Geez, you don't know me at all, do you? That's wrong.");
+
 alert("Thanks for playing, " + userName + ". You answered " + score + " out of 4 questions correctly.");
+}
